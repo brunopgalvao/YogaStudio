b6091664461cea7eaf03c0f0d4d029bbd6031c63
Bruno Galvao <brunopgalvao@gmail.com>
2018-04-20 14:52:10 -0400

    Use chromeDriver 2.37

--- protractor.conf.js
+++ protractor.conf.js
@@ -8,7 +8,7 @@ exports.config = {
   specs: [
     './e2e/**/*.e2e-spec.ts'
   ],
-  // chromeDriver: './node_modules/webdriver-manager/selenium/chromedriver_2.38',
+  chromeDriver: './node_modules/webdriver-manager/selenium/chromedriver_2.37',
   capabilities: {
     'browserName': 'chrome'
   },
