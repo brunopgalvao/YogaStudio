15e44768cdb92dc9e8524eaab5354c80f674b20b
Bruno Galvao <brunopgalvao@gmail.com>
2018-04-20 13:15:17 -0400

    Update markup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,13 +21,13 @@ describe('AppComponent', () => {
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('Yoga Studio');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to the Yoga Studio!');
   }));
   it('should render title in a mat-toolbar tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
