de842f02e5aeb5900051b40659c72f5b53ab3c9c
Bruno Galvao <brunopgalvao@gmail.com>
2018-04-20 13:32:48 -0400

    Fix e2e test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -18,7 +18,7 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'Yoga Studio'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Yoga Studio');
